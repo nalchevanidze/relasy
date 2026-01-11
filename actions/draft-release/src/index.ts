19b0866e123cebdde8b0eaf3a411211e296f2568
Daviti Nalchevanidze <d.nalchevanidze@gmail.com>
2026-01-11 13:29:15 +0100

    public version = () => this.module.version();

--- actions/draft-release/src/index.ts
+++ actions/draft-release/src/index.ts
@@ -3,17 +3,6 @@ import { Relasy } from "@relasy/core";
 
 async function run() {
   try {
-    const token = process.env.GITHUB_TOKEN || process.env.GITHUB_API_TOKEN;
-    if (!token) throw new Error("Missing GITHUB_TOKEN (or GITHUB_API_TOKEN).");
-
-    // provide both names for compatibility
-    process.env.GITHUB_TOKEN = process.env.GITHUB_TOKEN || token;
-    process.env.GITHUB_API_TOKEN = process.env.GITHUB_API_TOKEN || token;
-
-    // run in the checked-out repo
-    const cwd = process.env.GITHUB_WORKSPACE || process.cwd();
-    process.chdir(cwd);
-
     const easy = await Relasy.load();
 
     await easy.release();
--- packages/core/src/lib/relasy.ts
+++ packages/core/src/lib/relasy.ts
@@ -31,8 +31,21 @@ export class Relasy extends Api {
   }
 
   public static async load() {
+    const token = process.env.GITHUB_TOKEN || process.env.GITHUB_API_TOKEN;
+    if (!token) throw new Error("Missing GITHUB_TOKEN (or GITHUB_API_TOKEN).");
+
+    // provide both names for compatibility
+    process.env.GITHUB_TOKEN = process.env.GITHUB_TOKEN || token;
+    process.env.GITHUB_API_TOKEN = process.env.GITHUB_API_TOKEN || token;
+
+    // run in the checked-out repo
+    const cwd = process.env.GITHUB_WORKSPACE || process.cwd();
+    process.chdir(cwd);
+
     return new Relasy(await loadConfig());
   }
+
+  public version = () => this.module.version();
 
   private initialVersion = () => {
     const version = lastTag();
